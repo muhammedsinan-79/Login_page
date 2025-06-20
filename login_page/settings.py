6e644e5c5ea34d209c6c5dc3a2e963b84e70526a
muhammed sinan e <muhammedsinaan79@gmail.com>
2025-06-20 11:14:23 +0530

    Switch from mysqlclient to pymysql for Railway compatibility

--- login_page/settings.py
+++ login_page/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+pymysql.install_as_MySQLdb()
