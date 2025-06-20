760bac4001519193960b6e90215a1c889bf53c4a
muhammed sinan e <muhammedsinaan79@gmail.com>
2025-06-20 12:30:15 +0530

    change setting for modify databases for railwat

--- login_page/settings.py
+++ login_page/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import pymysql
-import os
+pymysql.install_as_MySQLdb()
+import os  
+from dotenv import load_dotenv
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,6 +104,7 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT", "3306"),
+
     }
 }
 
