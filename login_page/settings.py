dc8c971e8b4614dcd63d09a91dd3086e9a942f63
muhammed sinan e <muhammedsinaan79@gmail.com>
2025-06-20 18:48:19 +0530

    Switch from mysqlclient to pymysql for Railway

--- login_page/settings.py
+++ login_page/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 import os
 import dj_database_url
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
