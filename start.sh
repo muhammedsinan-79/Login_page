9c62aa821a593cbbd8aaa1b57d54c4c21948c360
muhammed sinan e <muhammedsinaan79@gmail.com>
2025-06-21 12:03:24 +0530

    Make start.sh executable

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn server..."
 gunicorn login_page.wsgi:application --bind 0.0.0.0:$PORT
